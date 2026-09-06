085170690da7f3cec2773dcdee5e8a403b117b5d
9037568 <namraccr@gmail.com>
2026-09-06 11:32:14 -0400

    Fix assertion expectation

--- DalSoft.RestClient.Test.Unit/RestClientTests.cs
+++ DalSoft.RestClient.Test.Unit/RestClientTests.cs
@@ -128,7 +128,7 @@ namespace DalSoft.RestClient.Test.Unit
             mockHttpClient.Verify(_ => _.Send
             (
                 HttpMethod.Get,
-                It.Is<Uri>(__ => __ == new Uri($"{BaseUri}{(callDynamically ? "/Users" : string.Empty)}?list=string&list=89&list=true")),
+                It.Is<Uri>(__ => __ == new Uri($"{BaseUri}{(callDynamically ? "/Users" : string.Empty)}?0=string&1=89&2=true")),
                 It.IsAny<IDictionary<string, string>>(),
                 It.IsAny<object>()
             ));
