ad80a287986c0fef641f6b0dcbd2f650b9ad9772
9037568 <namraccr@gmail.com>
2026-09-06 13:16:49 -0400

    Cleanup

--- DalSoft.RestClient.Test.Unit/RestClientTests.cs
+++ DalSoft.RestClient.Test.Unit/RestClientTests.cs
@@ -122,12 +122,10 @@ namespace DalSoft.RestClient.Test.Unit
 
             if (callDynamically)
             {
-                Console.WriteLine("Actual generated query: " + client.Users.Query(list));
                 await client.Users.Query(list).Get();
             }
             else
             {
-                Console.WriteLine("Actual generated query: " + ((IRestClient)client).Query(list));
                 await ((IRestClient)client).Query(list).Get();
             }
 
